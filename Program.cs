0f014c6b30678202d9b9574695480d9d1e925b11
MaxKhramtsov <maxkhramtsov13@gmail.com>
2023-01-25 10:40:48 +0300

    домашнне адание005

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿System.Console.Write($"Ввудите число - ");
+int a = Convert.ToInt32(Console.ReadLine());
+if( a % 2 == 0)
+System.Console.WriteLine($"Четное число");
+else
+System.Console.WriteLine($"Не четное число");
